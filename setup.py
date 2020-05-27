b38b5919ce8b2ea766fc96283845056653f80a7f
Nikhil Kothari <nikhil@retailio.in>
2020-05-27 18:36:32 +0530

    readme

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst", "r") as fh:
 
 
 setup(name='transly',
-      version='0.1.3',
+      version='0.1.4',
       description='Seq2Seq based model for transliteration, Hindi to English and English to Hindi',
       url='https://github.com/gitnik17/transly',
       author='Nikhil Kothari',

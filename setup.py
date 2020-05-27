d7774fadfa721de48005d06cfacff548ca8e27ba
Nikhil Kothari <nikhil@retailio.in>
2020-05-27 18:35:50 +0530

    readme

--- setup.py
+++ setup.py
@@ -11,13 +11,18 @@ with open("README.rst", "r") as fh:
 
 
 setup(name='transly',
-      version='0.1.2',
+      version='0.1.3',
       description='Seq2Seq based model for transliteration, Hindi to English and English to Hindi',
       url='https://github.com/gitnik17/transly',
       author='Nikhil Kothari',
       author_email='gitnik17@gmail.com',
       license='Apache License 2.0',
       zip_safe=False,
+      install_requires=[
+          'pandas',
+          'keras',
+          'tensorflow',
+      ],
       packages=['transly'],
       package_data={'transly': ['transly/*']},
       include_package_data=True,
